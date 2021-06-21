9d55328b7c3333f7e45d284d4587a713f5803d27
mlukica <mario.lukica@cloudsense.com>
2021-06-21 16:20:55 +0200

    Add demo dataset

---
+++ datasets/sample.sql
@@ -0,0 +1,27 @@
+BEGIN TRANSACTION;
+CREATE TABLE "Account" (
+	id INTEGER NOT NULL, 
+	"Name" VARCHAR(255), 
+	"ParentId" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Account" VALUES(1,'Sample Account for Entitlements','');
+INSERT INTO "Account" VALUES(2,'First Account','');
+CREATE TABLE "Delivery_Item__c" (
+	id INTEGER NOT NULL, 
+	"Food_Expiration_Date__c" VARCHAR(255), 
+	"Food_Storage__c" VARCHAR(255), 
+	"Name" VARCHAR(255), 
+	"Delivery__c" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Delivery_Item__c" VALUES(1,'2021-06-24','Non-refrigerated','First Delivery Item','1');
+CREATE TABLE "Delivery__c" (
+	id INTEGER NOT NULL, 
+	"Scheduled_Date__c" VARCHAR(255), 
+	"Status__c" VARCHAR(255), 
+	"Supplier__c" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Delivery__c" VALUES(1,'2021-06-24T19:00:00.000+0000','Requested','2');
+COMMIT;
